513169158a1646cd4e35b9755126aca8128d4617
shearstress021 <shearstress021@gmail.com>
2026-02-13 00:09:09 -0800

    connect localhost to port 8080

---
+++ src/main/java/com/example/pathfinder/controller/LandingController.java
@@ -0,0 +1,17 @@
+package com.example.pathfinder.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LandingController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("navbarType", "fragments/navbar :: navbar");
+        model.addAttribute("content", "landing/index :: content");
+        return "layout";
+    }
+}
